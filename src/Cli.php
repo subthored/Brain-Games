0199fe7e6f12072471b1871bb8bddcf36fec2bc9
Eugeny Zhikharev <janiqgames@gmail.com>
2024-01-23 02:19:28 +0700

    add src/Cli.php with new function

---
+++ src/Cli.php
@@ -0,0 +1,11 @@
+<?php
+
+use function cli\line;
+use function cli\prompt;
+
+function greeting()
+{
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
