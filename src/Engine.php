77d1ce2c4166dd50ed72c47f33ed0b82ebc5e9e4
Eugeny Zhikharev <janiqgames@gmail.com>
2024-01-27 21:49:35 +0700

    Simplifying code

--- src/Engine.php
+++ src/Engine.php
@@ -3,12 +3,23 @@
 use function cli\line;
 use function cli\prompt;
 
-function gameGreeting()
+function gameGreeting($game)
 {
-    line("Welcome to the Brain Games!");
-    $name = prompt("May I have your name?");
-    line("Hello, %s!", $name);
-    return $name;
+    if ($game === "even") {
+        line("Welcome to the Brain Games!");
+        $name = prompt("May I have your name?");
+        line("Hello, %s!", $name);
+        line("Answer 'yes' if the number is even, otherwise answer 'no'.\n");
+        return $name;
+    }
+
+    if ($game === "calc") {
+        line("Welcome to the Brain Games!");
+        $name = prompt("May I have your name?");
+        line("Hello, %s!", $name);
+        line("\nWhat is the result of the expression?");
+        return $name;
+    }
 }
 
 function compareAnswers($userAnswer, $correctAnswer, $name)
@@ -16,15 +27,13 @@ function compareAnswers($userAnswer, $correctAnswer, $name)
     if ($userAnswer == 0) {
         line("\nYou must type integer number!");
         line("Let's try again, %s", $name);
-        $isAnswerCorrect = false;
-        return $isAnswerCorrect;
+        return false;
     }
     if ($userAnswer !== $correctAnswer) {
         line("'%s' is wrong answer :( Correct answer was '%s'.", $userAnswer, $correctAnswer);
         line("\nLet's try again, %s!", $name);
-        $isAnswerCorrect = false;
+        return false;
     } else {
-        $isAnswerCorrect = true;
+        return true;
     }
-    return $isAnswerCorrect;
 }
