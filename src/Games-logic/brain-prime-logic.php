4991474964b32df2850909ed6c54194ec158d503
Eugeny Zhikharev <janiqgames@gmail.com>
2024-01-31 19:55:49 +0700

    added logic file for brain-prime game

---
+++ src/Games-logic/brain-prime-logic.php
@@ -0,0 +1,29 @@
+<?php
+
+use function cli\line;
+use function cli\prompt;
+
+function prime()
+{
+    $points = 0;
+    $isUserCorrect = true;
+
+    $name = gameGreeting("prime");
+
+    while ($isUserCorrect && $points < 3) {
+        $number = random_int(0, 100);
+        $correctAnswer = isNumberPrime($number);
+        line("Question %s", $number);
+        $userAnswer = prompt('Your answer');
+        $isUserCorrect = compareAnswers($userAnswer, $correctAnswer, $name);
+
+        if ($isUserCorrect) {
+            line("Correct!\n");
+            $points++;
+        }
+
+        if ($points === 3) {
+            line("Congratulations, %s!", $name);
+        }
+    }
+}
