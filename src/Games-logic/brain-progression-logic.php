fcdc9078848d07f3fc7770b69c5a7dd686551db7
Eugeny Zhikharev <janiqgames@gmail.com>
2024-01-31 19:14:00 +0700

    added logic for progression game

---
+++ src/Games-logic/brain-progression-logic.php
@@ -0,0 +1,35 @@
+<?php
+
+use function cli\line;
+use function cli\prompt;
+
+function progression()
+{
+    $points = 0;
+    $isUserCorrect = true;
+
+    $name = gameGreeting("progression");
+
+    while ($isUserCorrect && $points < 3) {
+        $firstNumber = random_int(1, 10);
+        $step = random_int(2, 5);
+        $length = random_int(5, 10);
+        $progression = generatingProgression($firstNumber, $step, $length);
+        $hiddenNumberIndex = random_int(0, count($progression) - 1);
+        $correctAnswer = $progression[$hiddenNumberIndex];
+        $progression[$hiddenNumberIndex] = '..';
+        line("Question: %s", implode(' ', $progression));
+
+        $userAnswer = (int)prompt('Your answer');
+        $isUserCorrect = compareAnswers($userAnswer, $correctAnswer, $name);
+
+        if ($isUserCorrect) {
+            line("Correct!\n");
+            $points++;
+        }
+
+        if ($points === 3) {
+            line("Congratulations, %s!", $name);
+        }
+    }
+}
